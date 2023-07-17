df98455060be282f7bb2cd705a5af9058afd9281
detallesceramicos <68312095+detallesceramicos@users.noreply.github.com>
2023-07-17 14:14:32 -0600

    push
    push 2

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Pyament Transanction',
+    'version': '1.0',
+    'author': 'Hunain',
+    'category': 'Finanance',
+    'summary': 'none',
+    'description': """
+    """,
+    'depends': [
+        'base',        
+        'hr',
+        'sale',
+        'account',
+        'purchase',
+        'portal'
+    ],
+    'data': [
+        'views/pt_payment_transaction_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    # 'post_init_hook': 'post_init_hook',
+    # 'settings': 'my_module_settings',
+}
